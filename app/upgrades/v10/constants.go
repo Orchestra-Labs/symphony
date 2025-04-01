06a3dc28403f74d2ebab8af4f638096ea589a62b
P-Yevhenii <pliutiak.yevhenii@gmail.com>
2025-04-01 14:12:04 +0300

    --Add upgrades app

---
+++ app/upgrades/v10/constants.go
@@ -0,0 +1,17 @@
+package v10
+
+import (
+	store "cosmossdk.io/store/types"
+	"github.com/osmosis-labs/osmosis/v26/app/upgrades"
+)
+
+const UpgradeName = "v10"
+
+var Upgrade = upgrades.Upgrade{
+	UpgradeName:          UpgradeName,
+	CreateUpgradeHandler: CreateUpgradeHandler,
+	StoreUpgrades: store.StoreUpgrades{
+		Added:   []string{},
+		Deleted: []string{},
+	},
+}
